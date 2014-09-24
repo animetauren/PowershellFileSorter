1647db355a0acbfbcaf526a00b761ecc2d37d7c1
Henry Robalino <hr28@njit.edu>
2014-09-24 10:55:34 -0500

    changed the file path to user's local path & added more ext's

--- FileSorter.ps1
+++ FileSorter.ps1
@@ -1,14 +1,14 @@
-﻿Param(
+Param(
     $sourcePath = '.\',
-    $destPath = 'C:\users\herobali\Documents\'
+    $destPath = [Environment]::GetFolderPath('MyDocuments')
 )
 
-$filePaths = @("$destPath\Docs", "$destPath\Apps", "$destPath\Etc", "$destPath\PDFs", "$destPath\Archive")
+$filePaths = @("$destPath\Docs", "$destPath\Apps", "$destPath\Etc", "$destPath\PDFs", "$destPath\Archive", "$destPath\Templates")
 
 $filePaths | % {
     if (!(Test-Path $_)) {
         mkdir $_
-        Write-Output "Docs Folder Created! `r`n"
+        Write-Output "Folder(s) Created! `r`n"
     }
 } 
 
@@ -20,12 +20,26 @@ Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".msi"} | Move-
 Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".docx"} | Move-Item -Destination $destPath\Docs
 Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".pptx"} | Move-Item -Destination $destPath\Docs
 Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".xlsx"} | Move-Item -Destination $destPath\Docs
-Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".jpg"} | copy-Item -Destination C:\users\herobali\Pictures\
-Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".jpg"} | Remove-Item #this is needed because copy-item complains about the path given to it. This is to make sure ther is no duplicates.
-Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".png"} | copy-Item -Destination C:\users\herobali\Pictures\
-Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".png"} | Remove-Item
-Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".pdf"} | Move-Item -Destination $destPath\PDFs
 Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".txt"} | Move-Item -Destination $destPath\Docs
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".jpg"} | copy-Item -Destination [Environment]::GetFolderPath('MyPictures')
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".jpg"} | Remove-Item #this is needed because copy-item complains about the path given to it. This is to make sure ther is no duplicates.
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".jpeg"} | copy-Item -Destination [Environment]::GetFolderPath('MyPictures')
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".jpeg"} | Remove-Item
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".png"} | copy-Item -Destination [Environment]::GetFolderPath('MyPictures')
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".png"} | Remove-Item
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".bmp"} | copy-Item -Destination [Environment]::GetFolderPath('MyPictures')
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".bmp"} | Remove-Item
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".aac"} | copy-Item -Destination [Environment]::GetFolderPath('MyMusic')
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".aac"} | Remove-Item
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".mp3"} | copy-Item -Destination [Environment]::GetFolderPath('MyMusic')
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".mp3"} | Remove-Item
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".avi"} | copy-Item -Destination [Environment]::GetFolderPath('MyVideos')
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".avi"} | Remove-Item
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".flv"} | copy-Item -Destination [Environment]::GetFolderPath('MyVideos')
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".flv"} | Remove-Item
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".mp4"} | copy-Item -Destination [Environment]::GetFolderPath('MyVideos')
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".mp4"} | Remove-Item
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".pdf"} | Move-Item -Destination $destPath\PDFs
 Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".zip"} | Move-Item -Destination $destPath\Archive
 Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".tar"} | Move-Item -Destination $destPath\Archive
 Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".vbs"} | Move-Item -Destination $destPath\Etc
@@ -34,5 +48,7 @@ Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".kdb"} | Move-
 Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".ics"} | Move-Item -Destination $destPath\Etc
 Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".xml"} | Move-Item -Destination $destPath\Etc
 Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".html"} | Move-Item -Destination $destPath\Etc
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".htm"} | Move-Item -Destination $destPath\Etc
 Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".encrypted"} | Move-Item -Destination $destPath\Etc
 Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".rdp"} | Move-Item -Destination $destPath\Etc
+Get-ChildItem -path $sourcePath | where-object {$_.Extension -eq ".vss"} | Move-Item -Destination $destPath\Templates
